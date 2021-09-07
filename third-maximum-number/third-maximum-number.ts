cf5e91cba56054ccc72ced4bd6ab65bd8843bfce
samqchau <67344952+samqchau@users.noreply.github.com>
2021-09-07 13:37:59 -0700

    Update third-maximum-number.ts

--- third-maximum-number/third-maximum-number.ts
+++ third-maximum-number/third-maximum-number.ts
@@ -8,18 +8,15 @@
 
 function thirdMax(nums: number[]): number {
     let maxes: number[] = new Array(3).fill(-Number.MAX_SAFE_INTEGER);
-    
     for(let i = 0; i < nums.length; i++) {
         let num:number = nums[i]
-        console.log(maxes)
         updateMaxArray(maxes, num)
-        console.log(maxes)
     }
 
     return maxes[0] > -Number.MAX_SAFE_INTEGER ? maxes[0] : maxes[2];
 };
 
-function updateMaxArray(arr: number[], num: number) {
+function updateMaxArray(arr: number[], num: number): void {
     for(let i = arr.length - 1; i >= 0; i--) {
         if(num === arr[i]) break;
         if(num > arr[i]) {
