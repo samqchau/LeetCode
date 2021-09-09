8847e57ce48f3cc36fd13d38ddfd48757ee0f389
samqchau <67344952+samqchau@users.noreply.github.com>
2021-09-09 11:09:50 -0700

    Time: 110 ms (23.85%), Space: 39.7 MB (94.50%) - LeetHub

---
+++ middle-of-the-linked-list/middle-of-the-linked-list.ts
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ 
+   Time Complexity - O(n/2) --> O(n)
+   Linked lists have linear traversal. As the input size of the linked list increases, traversal time increases at a rate of (n/2)
+   
+   Space Complexity - O(1)
+   Space requirements are independent from linked list input size
+   
+ */
+
+function middleNode(head: ListNode | null): ListNode | null {
+    let p1:ListNode | null = head;
+    let p2:ListNode | null = head;
+    while(p2 !== null) {
+        if(p2.next === null) break;
+        p1 = p1.next;
+        p2 = p2.next.next;
+    }
+    return p1
+};
