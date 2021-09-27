85cd5f088031eeb025e9ec1d6262c2ab7721ae1f
samqchau <67344952+samqchau@users.noreply.github.com>
2021-09-27 12:58:14 -0700

    Update find-center-of-star-graph.ts

--- find-center-of-star-graph/find-center-of-star-graph.ts
+++ find-center-of-star-graph/find-center-of-star-graph.ts
@@ -1,8 +1,4 @@
 /*
-    Approaches
-    1. Iterate through edges. Remember last edge. Check to see what node both prevEdge and currEdge are both are connected to. If this graph is guarenteed to be a star graph, then the common node of the pairs would be the center of the star.
-    2. Create an adjacency list - the node with more than 1 connection is the center of the star or every other node's only neighbor is the center.
-    
     Time Complexity - O(1)
     Space Complexity - O(1);
 */
