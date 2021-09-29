942f9cf695adcb020b667917ddb17e135bf49a49
samqchau <67344952+samqchau@users.noreply.github.com>
2021-09-29 14:19:20 -0700

    Update jump-game.ts

--- jump-game/jump-game.ts
+++ jump-game/jump-game.ts
@@ -1,13 +1,19 @@
 /*
+    Time Complexity - O(n)
+    Jump from each index once. If we've already jumped there it's constant look up.
+    
+    Space Complexity - O(n)
+    Recursive calls use implicit stack space, in the worst case it could be length n.
+    Use an array of booleans size n.
+    
     Approach
-    We start at square 0 so it's marked as true. We are going to mark every square that we can jump to as true. Jump as far as we can and then mark everything up to that point as true. If we find something that was true, everything before that is true. If our position === nums.length - 1 we can return early.
+    We start at square 0. We mark every square we can jump to as true. Jump as far as we can recursively returning early if we reach the end. If we still haven't found the end, check the lesser jump distances. If our position === nums.length - 1 we can return early.
 */
 
 function canJump(nums: number[]): boolean {    
     let n = nums.length;
     let isJumpable = new Array(n).fill(false);
     return maxJump(0) ? true : isJumpable[n-1];
-    return 
     
     function maxJump(pos) {
         if(pos >= n - 1) return true;
