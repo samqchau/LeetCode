4730da029fa1a67ae58e69f66f9db1582749daf9
samqchau <67344952+samqchau@users.noreply.github.com>
2021-09-30 16:08:32 -0700

    Time: 1680 ms (15.98%), Space: 84.6 MB (5.95%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.ts
+++ longest-palindromic-substring/longest-palindromic-substring.ts
@@ -1,26 +1,23 @@
-/*
-    Time Complexity - O(n^2)
-
-    Space Complexity - O(n)
-*/
 
 function longestPalindrome(s: string): string {
     let n = s.length;
+    if(n < 2) return s;
     let res = '';
-    for(let i = 0; i < n; i++) {
-        expandPalindrome(i, i);
-        if(s[i] === s[i+1]){
-            expandPalindrome(i, i+1);
-        }
-    }
-    return res;
+    let dp = new Array(n + 1).fill(0).map(_ => new Array(n + 1).fill(false));
     
-    function expandPalindrome(l, r) {
-        let string = s.slice(l, r + 1);        
-        while(l >= 0 && r < n && s[l] === s[r]) {
-            if(string.length > res.length) res = string;
-            l--; r++;
-            string = `${s[l]}${string}${s[r]}`;
+    for(let i = 0; i < n; i++) {
+        dp[i][i] = true;
+        res = s[i];
+    }
+    for(let i = 0; i < n - 1; i++) {
+        if(s[i] === s[i+1]) dp[i][i+1] = true;
+        if(dp[i][i+1]) res = s.substring(i, i+2);
+    }
+    for(let i = n - 1; i >= 0; i--) {
+        for(let j = i + 2; j <= n; j++) {
+            dp[i][j] = s[i] === s[j] && dp[i+1][j-1];
+            if(dp[i][j] && j-i + 1 > res.length) res = s.substring(i, j+1);
         }
     }
+    return res
 };
