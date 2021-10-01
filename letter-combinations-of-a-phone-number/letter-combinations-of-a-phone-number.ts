b287f2812964f29a7f805ec27e07dda0c50fbe4c
samqchau <67344952+samqchau@users.noreply.github.com>
2021-10-01 13:53:40 -0700

    Time: 142 ms (5.54%), Space: 40.4 MB (61.54%) - LeetHub

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.ts
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.ts
@@ -1,31 +1,25 @@
-/*
-    Time Complexity - O(4^n * n)
-    4 Recursive calls for each letter a key number can represent and 4 more for each number after that for the entire length of the input digits string.
-    
-    Space Complexity - O(n)
-    DFS traversal of tree uses implicit stack space with the height of the tree which is the length of the string.
-    
-*/
-
 function letterCombinations(digits: string): string[] {
-    let res:string[] = [];
-    let letters = {
-        2: ['a','b','c'],
-        3: ['d', 'e', 'f'],
-        4: ['g','h','i'],
-        5: ['j','k','l'],
-        6: ['m','n','o'],
-        7: ['p','q','r','s'],
-        8: ['t','u','v'],
-        9: ['w','x','y','z'],
+    if(digits.length === 0) return []
+    let words = [];
+    let map = {
+        '2': ['a','b','c'],
+        '3': ['d','e','f'],
+        '4': ['g','h','i'],
+        '5': ['j','k','l'],
+        '6': ['m','n','o'],
+        '7': ['p','q','r','s'],
+        '8': ['t','u','v'],
+        '9': ['w','x','y','z']
     }
-    if(digits.length === 0) return res
-    function helper (s) {
-        if(s.length === digits.length) {res.push(s); return}
-        let key = digits[s.length];
-        letters[key].forEach(l => helper(s + l))
-    }
+
+    helper('');
+    return words;
     
-    helper('')
-    return res
+    function helper(s) {
+        if(s.length === digits.length) words.push(s);
+        let num = `${digits[s.length]}`;
+        if(map[num]) {
+            map[num].forEach(n => helper(s + n));
+        }        
+    }
 };
