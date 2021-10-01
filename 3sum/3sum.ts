61696fbf40500a566f7a8d8e6605b320d235044f
samqchau <67344952+samqchau@users.noreply.github.com>
2021-10-01 14:09:28 -0700

    Time: 444 ms (35.96%), Space: 52.2 MB (31.92%) - LeetHub

--- 3sum/3sum.ts
+++ 3sum/3sum.ts
@@ -1,34 +1,28 @@
-/*
-    Time Complexity - O(n^2)
-    -sort O(nlogn)
-    -for loop with nested while loop O(n^2)
-    
-    Space Complexity - O(n)
-    -results array O(n)
-    -map to keep track of triplets O(n)
-*/
+
 function threeSum(nums: number[]): number[][] {
-    let res = []; let sets = {};
-    if(nums.length < 3) return res
-    nums.sort((a,b) => a - b);
+    if(nums.length < 3) return [];
+    let res = []; let map = {};
+    
+    nums.sort((a,b) => {return a-b});
     for(let i = 0; i < nums.length; i++) {
-        let j = i + 1;
-        let k = nums.length - 1;
-        while(j < k) {
-            let sum = nums[i] + nums[j] + nums[k];
+        let l = i+1, r = nums.length - 1;
+        while(l < r) {
+            let sum = nums[i] + nums[l] + nums[r];
             if(sum < 0) {
-                j++;
-            } else if (sum > 0) {
-                k--;
+                l++;
+            }
+            else if (sum > 0) {
+                r--;
             } else {
-                let key = `${nums[i]},${nums[j]},${nums[k]}`
-                if(!sets[key]) {
-                    res.push([nums[i], nums[j], nums[k]]);
-                    sets[key] = true;
+                let key = `${nums[i]},${nums[l]},${nums[r]}`;
+                if(!map[key]) {
+                    map[key] = true;
+                    res.push([nums[i], nums[l], nums[r]]);
                 }
-                j++; k--;
+                l++; r--;
             }
         }
     }
+    
     return res;
 };
