fb6d8aaf039a046a7bb5ccf3ef67e08e888080ce
samqchau <67344952+samqchau@users.noreply.github.com>
2021-10-01 18:14:38 -0700

    Time: 84 ms (100.00%), Space: 43.6 MB (47.83%) - LeetHub

---
+++ n-queens/n-queens.ts
@@ -0,0 +1,62 @@
+/*
+    Place queens
+*/
+
+function solveNQueens(n: number): string[][] {
+    let solutions = [];
+    for(let i = 0; i < n; i++) {
+        placeQueen(n, solutions, [{row: i, col: 0}], 1);
+    }
+    return solutions
+};
+
+function validRow (row, queens) {
+    for(let i = 0; i < queens.length; i++) {
+        let q = queens[i];
+        if(row === q.row) return false;
+    }
+    return true
+}
+
+function validCol(col, queens) {
+    for(let i = 0; i < queens.length; i++) {
+        let q = queens[i];
+        if(col === q.col) return false;
+    }
+    return true
+}
+
+function validDiag(n, row, col, queens) {
+    for(let i = 0; i < queens.length; i++) {
+        let q = queens[i], r = q.row, c = q.col;
+        for(let j = 0; j < n; j++) {
+            let top = row - j, bottom = row + j, left = col - j, right = col + j;
+            if(
+                (r===top && c === left) ||
+                (r===bottom && c === left) ||
+                (r===top && c === right) ||
+                (r===bottom && c === right)
+            ) return false
+        }
+    }
+    return true;
+}
+
+function placeQueen(n, res, queens = [], col) {
+    if(queens.length === n) {
+        res.push(generateBoard(n, queens));
+        return;
+    }
+    for(let row = 0; row < n; row++) {
+        if(validRow(row, queens) && validCol(col, queens) && validDiag(n, row, col, queens))
+            placeQueen(n, res, [...queens, {row, col: col}], col + 1);
+    }
+}
+
+function generateBoard (n, queens) {
+    let board = new Array(n).fill(0).map(_ => new Array(n).fill('.'));
+    queens.forEach(q => {
+        board[q.row][q.col] = 'Q'
+    })
+    return board.map(row => row.join(''));
+}
