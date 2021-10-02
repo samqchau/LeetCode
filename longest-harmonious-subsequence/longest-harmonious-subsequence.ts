3c67bc8464729d0bab052c50c3adfd12aa5a20ba
samqchau <67344952+samqchau@users.noreply.github.com>
2021-10-02 12:50:54 -0700

    Time: 140 ms (31.82%), Space: 52.3 MB (9.09%) - LeetHub

--- longest-harmonious-subsequence/longest-harmonious-subsequence.ts
+++ longest-harmonious-subsequence/longest-harmonious-subsequence.ts
@@ -1,35 +1,15 @@
-/*
-    Time Complexity - O(n^2)
-    Two nested for loops
-    
-    Space Complexity - O(1)
-    Space independent from input size
-*/
 function findLHS(nums: number[]): number {
-    let res = 0;
+    let map = {}, res = 0;
     for(let i = 0; i < nums.length; i++) {
-        if(res > nums.length - i) return res
-        let num = nums[i];
-        let minCount = 0;
-        let maxCount = 0;
-        let count = 0;
-        let valid = false;
-        for(let j = i; j < nums.length; j++) {
-            if(nums[j] === num) {
-                count++;
-                continue;
-            }
-            if(nums[j] - num === 1) {
-                minCount++;
-                valid = true;
-            } else if (num - nums[j] === 1) {
-                maxCount++;
-                valid = true;
-            }
-        }
-        if(valid) {
-            res = Math.max(res, Math.max(minCount, maxCount) + count)
-        }
+        map[nums[i]] ? map[nums[i]]++ : map[nums[i]] = 1;
     }
-    return res
+    
+    for(let key of Object.keys(map)) {
+        let num = Number(key), highestNeighborCount = -Infinity;
+        if(map[num - 1]) highestNeighborCount = map[num-1];
+        if(map[num + 1] && map[num + 1] > highestNeighborCount) highestNeighborCount = map[num + 1];
+        let sequenceLength = highestNeighborCount + map[key]
+        sequenceLength > res ? res = sequenceLength : null;
+    }
+    return res;
 };
