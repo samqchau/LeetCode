441b56046fe93ba175bb4b3060e0ec0a38d1235d
samqchau <67344952+samqchau@users.noreply.github.com>
2021-10-03 14:20:23 -0700

    Update 3sum.ts

--- 3sum/3sum.ts
+++ 3sum/3sum.ts
@@ -1,3 +1,22 @@
+/*
+    -- Approach --
+    Sort the nums array.
+    Treat the array like a window, for each iteration, we will shrink the window size by one.
+    The left most number is fixed. There will also be a left and right pointer.
+    Now that three numbers are selected, check the sum.
+    If the sum is equal to the target value and is new, add the three numbers to the results array and add their key to a hashmap.
+    Else if the sum is greater than the target value move the left pointer right
+    Else if the sum is less than the target value move the right pointer left
+    
+    Time Complexity - O(n^2)
+    -nlog(n) sort
+    -Shrinking window is produces a triangular time complexity n*(n-1) * 1/2.
+    
+    Space Complexity - O(n)
+    -Results array
+    -Boolean map
+*/
+
 
 function threeSum(nums: number[]): number[][] {
     if(nums.length < 3) return [];
