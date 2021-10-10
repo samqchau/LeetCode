935303707ceab742cd8f969e85896b7d001ec6f5
samqchau <67344952+samqchau@users.noreply.github.com>
2021-10-10 10:55:26 -0700

    Time: 80 ms (91.71%), Space: 42.4 MB (86.34%) - LeetHub

--- majority-element/majority-element.ts
+++ majority-element/majority-element.ts
@@ -1,4 +1,10 @@
 /*
+    Approaches
+    Sort the array O(nlogn)
+    Return the element at index Math.floor(arr.length / 2)
+
+    We can store a running count that increases when we see a repeating candidate, if the number is not the candidate, we decrease the running count and if the running count hits 0 we set a new candidate. After we iterate through the entire array, the majority element will be the candidate. Return the candidate
+    
     Time Complexity - O(n)
     Iterate through the numbers array one time
     
@@ -7,17 +13,17 @@
 */
 
 function majorityElement(nums: number[]): number {
-    let count:number = 1;
-    let candidate:number = nums[0];
+    let count = 1, candidate = nums[0];
     for(let i = 1; i < nums.length; i++) {
-        if(nums[i] === candidate) {
-            count++;
-            continue;
-        } else if(count > 0) {
+        let n = nums[i];
+        if(n === candidate) {count++; continue;}
+        else {
             count--;
-        } else {
-            count = 1;
-            candidate = nums[i];
+            if(count === 0) {
+                candidate = n;
+                count++;
+                continue;
+            }
         }
     }
     return candidate
