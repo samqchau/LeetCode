a8faea022a53bb2020d61f452b4ec52d06ba2827
samqchau <67344952+samqchau@users.noreply.github.com>
2021-10-26 16:20:45 -0700

    Time: 72 ms (85.71%), Space: 40.6 MB (28.57%) - LeetHub

--- remove-palindromic-subsequences/remove-palindromic-subsequences.ts
+++ remove-palindromic-subsequences/remove-palindromic-subsequences.ts
@@ -1,51 +1,21 @@
 /*
-    **Interpreted the question wrong and wrote a solution for removing longest palindromic substring, not subsequence. Because the entire string is only made of a's and b's, if we remove all the a's, only b's will be left. On the next iteration all b's will be removed therefore the maximum result will only ever be 2. If there were 3, 4, ...k elements instead, the maximum answer would be k.
-    
     Approach
-    res = 0
-    While string still has length
-        n = s.length
-        create a sliding window to check to see if string is palindrome
-            if string is a palindrome
-                split the string.
-                splice from l to r adding no elements
-                join the string
-                s = string
-                increment res
-                break;
+    if the string is a palindrome.
+        return 1
+    else return 2
     
-    return res
-    
-    Time Complexity - O(n**2)
-    Space Complexity - O(n)
+    Time Complexity - O(n)
+    Space Complexity - O(1)
 */
 
 function removePalindromeSub(s: string): number {
-    let res = 0;
-    while(s.length) {
-        let foundPalindrome = false;
-        for(let j = s.length; j >= 0; j--) {
-            for(let i = 0; i < s.length; i++) { 
-            let ss = s.substring(i, j);
-                if(isPalindrome(ss)) {
-                    let str = ss.split('');
-                    str.splice(i, j)
-                    s = str.join('');
-                    foundPalindrome = true;
-                    res++;
-                    break;
-                }
-            }
-            if(foundPalindrome) break;
-        }
-    }
-    return res >= 2 ? 2 : 1;
+    return isPalindrome(s) ? 1 : 2
 };
 
-function isPalindrome (str) {
-    let l = 0, r = str.length - 1;
+function isPalindrome(s) {
+    let l = 0, r = s.length - 1;
     while(l < r) {
-        if(str[l] !== str[r]) return false
+        if(s[l] !== s[r]) return false
         l++; r--;
     }
     return true
