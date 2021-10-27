491f19cd8308cf73375dfdaeffaae6ece7600cde
samqchau <67344952+samqchau@users.noreply.github.com>
2021-10-26 18:28:28 -0700

    Time: 144 ms (65.52%), Space: 48.9 MB (10.34%) - LeetHub

--- find-k-closest-elements/find-k-closest-elements.ts
+++ find-k-closest-elements/find-k-closest-elements.ts
@@ -3,38 +3,9 @@
     Build a heap where the priority element is the closest number to k;
     add k elements to result
     return result
-    
-    Heap Implementation
-    [1,2,3,4,5]
-    Use an array where
-    n
-    l = 2n + 1;
-    r = 2n + 2;
-    
-    What is a heap
-    Heap is a data structure where the top element is guarenteed to be the priority
-    We need to swap this element with the last
-    Now the top element is guarenteed to belong on the last level of the heap
-    Bubble down the number
-    Do it again
-    
-    Functions
-    
-    turn arr into heap
-    heapify (){
-        for every element starting from the right
-            bubbleDown
-    }
-    
-    bubbleDown () {
-        if node's left or right is higher priority
-            (x-node.val <= x-node.left.val)
-            swap();
-    }
-    
-    swap () {
-        
-    }
+
+    Time Complexity - O(nlogn + klogn + klogk)
+    Space Complexity - O(k)
 */
 
 function findClosestElements(arr: number[], k: number, x: number): number[] {
@@ -47,12 +18,14 @@ function findClosestElements(arr: number[], k: number, x: number): number[] {
     return res.sort((a,b) => {return a - b})
 };
 
+//nlogn
 function heapify (arr, l, r, x) {
     for(let i = r; i >= 0; i--) {
         bubbleDown(arr, i, r, x);
     }
 }
 
+//Log n
 function bubbleDown (arr, i, end, x) {
     let n = i, nnum = arr[n], ndist = Math.abs(x-nnum);
     let l = 2 * i + 1, r = 2 * i + 2;
@@ -64,7 +37,7 @@ function bubbleDown (arr, i, end, x) {
     if(rdist < ldist || (rdist === ldist && rnum < lnum)) {min = rdist; minIdx = r;}
     if(min < ndist) {[arr[minIdx], arr[n]] = [arr[n], arr[minIdx]]; bubbleDown(arr, minIdx, end, x)}
 }
-
+//logn
 function pop (arr, x) {
     [arr[0], arr[arr.length - 1]] = [arr[arr.length - 1], arr[0]];
     let res = arr.pop();
