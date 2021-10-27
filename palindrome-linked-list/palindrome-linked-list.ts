df4db6fb7bf9684bfc7b974fef25888ca66fbb04
samqchau <67344952+samqchau@users.noreply.github.com>
2021-10-26 23:20:10 -0700

    Time: 200 ms (69.95%), Space: 64.5 MB (84.97%) - LeetHub

--- palindrome-linked-list/palindrome-linked-list.ts
+++ palindrome-linked-list/palindrome-linked-list.ts
@@ -1,24 +1,22 @@
-/*  
-    Time Complexity - O(n)    
+/*
+    Time Complexity - O(n)
     Space Complexity - O(n)
- */
-
+*/
 function isPalindrome(head: ListNode | null): boolean {
-    let n = 0;
-    let p1 = head;
-    while(p1) {n++; p1 = p1.next;}
-    let mid = Math.ceil(n / 2);
-    
-    p1 = head; let p2 = head, stack = [];
-    while(mid--) {
-        stack.push(p2.val);
-        p2 = p2.next;
+    let slow = head, fast = head;
+    while(fast.next && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
     }
-    if(n % 2 === 1) stack.pop();
-    
-    while(p2) {
-        if(p2.val !== stack.pop()) return false
-        p2 = p2.next;
+    let stack = [];
+    while(slow) {
+        stack.push(slow.val);
+        slow = slow.next;
     }
-    return true;
+    slow = head;
+    while(stack.length) {
+        if(stack.pop() !== slow.val) return false
+        slow = slow.next;
+    }
+    return true
 };
