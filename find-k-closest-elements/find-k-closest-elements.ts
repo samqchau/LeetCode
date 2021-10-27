589dcaef6db48bb298b20ea6647d1f3bff839036
samqchau <67344952+samqchau@users.noreply.github.com>
2021-10-26 18:50:52 -0700

    Time: 112 ms (82.76%), Space: 47 MB (24.14%) - LeetHub

--- find-k-closest-elements/find-k-closest-elements.ts
+++ find-k-closest-elements/find-k-closest-elements.ts
@@ -1,46 +1,39 @@
 /*
-    Approach
-    Build a heap where the priority element is the closest number to k;
-    add k elements to result
-    return result
-
-    Time Complexity - O(nlogn + klogn + klogk)
+    Time Complexity - O(n + k + klogk) --> O(n ** 2)
     Space Complexity - O(k)
 */
-
 function findClosestElements(arr: number[], k: number, x: number): number[] {
-    heapify(arr, 0, arr.length - 1, x);
-    let res = []
+    let res = [];
+    let i = closestIndex(arr, x);
+    res.push(arr[i]); k--;
+    let l = i - 1, r = i + 1;
     while(k--) {
-        let num = pop(arr, x);
-        res.push(num);
+        if(l >= 0 && r <= arr.length - 1) {
+            if(Math.abs(x - arr[l]) <= Math.abs(x - arr[r])) {
+                res.push(arr[l]);
+                l--;
+            } else {
+                res.push(arr[r]);
+                r++;
+            }
+        } else if(l >= 0) {
+            res.push(arr[l]);
+            l--;
+        } else {
+            res.push(arr[r]);
+            r++;
+        }
     }
-    return res.sort((a,b) => {return a - b})
+    return res.sort((a,b) => {return a-b})
 };
 
-//nlogn
-function heapify (arr, l, r, x) {
-    for(let i = r; i >= 0; i--) {
-        bubbleDown(arr, i, r, x);
+function closestIndex(arr, x) {
+    let min = Infinity, minIndex = -1;
+    for(let i = 0; i < arr.length; i++) {
+        if(Math.abs(x - arr[i]) < min) {
+            min = Math.abs(x - arr[i]);
+            minIndex = i;
+        }
     }
-}
-
-//Log n
-function bubbleDown (arr, i, end, x) {
-    let n = i, nnum = arr[n], ndist = Math.abs(x-nnum);
-    let l = 2 * i + 1, r = 2 * i + 2;
-    if(n > end) return
-    
-    let lnum = arr[l], ldist = Math.abs(x-lnum);
-    let rnum = arr[r], rdist = Math.abs(x-rnum);
-    let min = ldist, minIdx = l;
-    if(rdist < ldist || (rdist === ldist && rnum < lnum)) {min = rdist; minIdx = r;}
-    if(min < ndist) {[arr[minIdx], arr[n]] = [arr[n], arr[minIdx]]; bubbleDown(arr, minIdx, end, x)}
-}
-//logn
-function pop (arr, x) {
-    [arr[0], arr[arr.length - 1]] = [arr[arr.length - 1], arr[0]];
-    let res = arr.pop();
-    bubbleDown(arr, 0, arr.length - 1, x);
-    return res;
+    return minIndex;
 }
