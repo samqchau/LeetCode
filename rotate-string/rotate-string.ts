d36d05690a84b729c933b30276e03cacd9c9dd0f
samqchau <67344952+samqchau@users.noreply.github.com>
2021-10-28 15:09:46 -0700

    Time: 80 ms (68.42%), Space: 40.6 MB (36.84%) - LeetHub

--- rotate-string/rotate-string.ts
+++ rotate-string/rotate-string.ts
@@ -1,17 +1,15 @@
 /*
-    Time Complexity - O(n^2)
-    Two nested for loops
-    
+    Approach
+    Time Complexity - O(n**2)
     Space Complexity - O(1)
-    Space requirements do not grow with input size
 */
 function rotateString(s: string, goal: string): boolean {
+    if(s.length !== goal.length) return false
     let n = s.length;
     for(let i = 0; i < n; i++) {
         if(s[0] === goal[i]) {
             for(let j = 0; j < n; j++) {
-                let p2 = i + j >= n ? (i+j) % n : i + j
-                if(s[j] !== goal[p2]) break;
+                if(s[j] !== goal[i + j > n - 1 ? (i + j) % n : i + j]) break;
                 if(j === n - 1) return true
             }
         }
