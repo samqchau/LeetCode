ab52e1dcef7be69a0ff03fa22cb057947ff0a845
Samuel Chau <67344952+samqchau@users.noreply.github.com>
2022-01-09 13:01:53 -0800

    Time: 317 ms (14.89%), Space: 48.8 MB (18.63%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.ts
+++ 217-contains-duplicate/217-contains-duplicate.ts
@@ -1,3 +1,7 @@
 function containsDuplicate(nums: number[]): boolean {
-    return nums.length > new Set(nums).size ? true : false;
+    nums.sort((a,b) => a-b);
+    for(let i = 0; i < nums.length - 1; i++) {
+        if(nums[i] === nums[i+1]) return true
+    }
+    return false
 };
