643af386baabc3e76bc3ce0ff4cbb21f9ab9b68a
Samuel Chau <67344952+samqchau@users.noreply.github.com>
2022-01-10 15:49:52 -0800

    Time: 120 ms (7.14%), Space: 38.7 MB (63.46%) - LeetHub

--- 198-house-robber/198-house-robber.js
+++ 198-house-robber/198-house-robber.js
@@ -5,8 +5,9 @@
 var rob = function(nums, i = 0, memo = {}) {
     if(memo[i] !== undefined) return memo[i]
     if (i > nums.length - 1) return 0
-    let skipHouse = rob(nums, i + 1, memo)
     let robHouse = rob(nums, i + 2, memo) + nums[i]
+    let skipHouse = rob(nums, i + 1, memo)
     memo[i] = Math.max(robHouse, skipHouse)
+    
     return memo[i]
 };
