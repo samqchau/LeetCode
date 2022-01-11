4e0ae156b258a6bff9a7ddf2d6a5b66ecdd2682c
Samuel Chau <67344952+samqchau@users.noreply.github.com>
2022-01-10 18:28:18 -0800

    Time: 72 ms (96.43%), Space: 41.7 MB (60.92%) - LeetHub

---
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    function dfs(node, depth = 0) {
+        if(!node) return depth
+        return Math.max(dfs(node.left, depth + 1), dfs(node.right, depth + 1))
+    }
+    return dfs(root)
+}
