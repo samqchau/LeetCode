47c154c4f7f7d87ec410e1d789b6550d2485cdcf
Samuel Chau <67344952+samqchau@users.noreply.github.com>
2022-01-30 22:34:48 -0800

    Time: 177 ms (43.92%), Space: 49.1 MB (10.60%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,19 @@
+var lengthOfLongestSubstring = function(s) {
+    if(s.length === 0) return 0
+    else if (s.length === 1) return 1
+    
+    let l = 0, r = 0, longest = 1, chars = {};
+
+    while(l < s.length && r < s.length) {
+        if(!chars[s[r]]) {
+            chars[s[r]] = true;
+            longest = Math.max(longest, r - l + 1);
+            r++;
+        } else {
+            chars[s[l]] = false;
+            l++;
+        }
+    }
+    return longest
+    
+};
